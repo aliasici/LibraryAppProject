5ea6f7818952fd2687d8e63d5f9c95da7d2e3209
aliasici <asiciali@yahoo.com>
2020-11-01 22:43:15 -0800

    I debug some codes

--- src/test/java/com/library/runners/CucumberRunner.java
+++ src/test/java/com/library/runners/CucumberRunner.java
@@ -2,8 +2,17 @@ package com.library.runners;
 
 
 import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = "com/library/step_definitions",
+        features = "src/test/resources/features",
+        dryRun = true,
+        monochrome=true,
+        plugin = {"pretty","html:test-output"}
+
+)
 public class CucumberRunner {
 }
